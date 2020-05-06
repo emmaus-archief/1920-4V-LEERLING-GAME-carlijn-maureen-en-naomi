d6a096891c231660ccd0eb4ed46ed0e8ec5995ab
naomilubeek <i.am.naomi04@icloud.com>
2020-05-06 13:39:51 +0000

    achtergrond-is-roze-hehe

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ var score = 0; // aantal behaalde punten
  * Tekent het speelveld
  */
 var tekenVeld = function () {
-  fill("purple");
+  fill('pink');
   rect(20, 20, width - 2 * 20, height - 2 * 20);
 };
 
